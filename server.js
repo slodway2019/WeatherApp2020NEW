386b0b0570c5b125816df2a2f52fdace8e8958a3
Surenkhuu Shagdarsuren <surenkhuu@gmail.com>
2020-10-11 22:02:55 -0700

    Initial Set Up

---
+++ server.js
@@ -0,0 +1,18 @@
+ if (process.env.NODE_ENV !== 'production') {
+     require('dotenv').config()
+ }
+
+ const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY
+ const express = require('express')
+ const app = express()
+
+ app.use(express.json())
+ app.use(express.static('public'))
+
+ app.post('/weather', (req, res) => {
+
+ })
+
+ app.listen(3000, () => {
+     console.log('Server Started')
+ })
